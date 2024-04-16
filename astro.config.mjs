7bc55ca1b7c2913ccc02be6c0e025b24b8d2ac10
AlexisSM377 <al222111377@gmail.com>
2024-04-15 23:20:20 -0600

    landing ui complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import auth from "auth-astro";
+import lottie from "astro-integration-lottie";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth()],
+  integrations: [tailwind(), auth(), lottie()],
   output: "server",
   adapter: vercel()
 });
